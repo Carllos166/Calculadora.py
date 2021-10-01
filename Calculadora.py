a59cba1141a7f75c71125a8f89b0f62cd21eb2aa
Carlos <75595248+Carllos166@users.noreply.github.com>
2021-10-01 17:51:29 -0300

    Consegui realizar o loop

--- Calculadora.py
+++ Calculadora.py
@@ -1,24 +1,36 @@
+print("---------------------------------------------------------")
+print("--------------- C A L C U L A D O R A -------------------")
+print("---------------------------------------------------------")
 
-calculadora = print(input("Digite (S) para continuar:  ").upper())
+deseja_continuar = 0
+rodada = 1
+while (rodada > 0):
 
-while calculadora == "S":
+    num1 = int(input("Entre num1: "))
+    num2 = int(input("Entre num2: "))
+    acao = str(input("Escolha entre (a), (s), (m), (d) para exeturar sua acao: Somar(a), Subtrair(s), Multiplicar(m), Dividir(d) -> ").upper())
+    
+    print("O resultado é ",end="")
+    if acao == "A":
+        print(num1 + num2)
+    elif acao == "S":
+        print(num1 - num2)
+    elif acao == "M":
+        print(num1 * num2)
+    else:
+        print(num1 / num2)
+
     print("--------------------------------------------")
-    break
-else:
-    print("Obrigado por participar do meu projeto.")
 
-num1 = int(input("Entre num1: "))
-num2 = int(input("Entre num2: "))
-acao = str(input("Escolha entre (a), (s), (m), (d) para exeturar sua acao: Somar(a), Subtrair(s), Multiplicar(m), Dividir(d) -> "))
+    deseja_continuar = input("Digite (1) para continuar ou (0) para sair:  ")
+    rodada = int(deseja_continuar)
 
-print("O resultado é ",end="")
-if acao == "a":
-    print(num1 + num2)
-elif acao == "s":
-    print(num1 - num2)
-elif acao == "m":
-    print(num1 * num2)
-else:
-    print(num1 / num2)
 
-print("--------------------------------------------")
+
+
+
+
+print("Obrigado por usar meu programa")
+
+
+
