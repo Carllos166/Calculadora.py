4843d9bb686d2db9aa8dbcff41be5c7e7bc04276
Carlos <75595248+Carllos166@users.noreply.github.com>
2021-10-01 18:52:04 -0300

    Adicionando raiz quadrada.

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,5 @@
+import math
+
 print("---------------------------------------------------------")
 print("--------------- C A L C U L A D O R A -------------------")
 print("---------------------------------------------------------")
@@ -8,7 +10,7 @@ while (rodada > 0):
 
     num1 = int(input("Entre num1: "))
     num2 = int(input("Entre num2: "))
-    acao = str(input("Escolha entre (a), (s), (m), (d) para exeturar sua acao: Somar(a), Subtrair(s), Multiplicar(m), Dividir(d) -> ").upper())
+    acao = str(input("Escolha entre (a), (s), (m), (d), (r) para exeturar sua acao: Somar(a), Subtrair(s), Multiplicar(m), Dividir(d), Raiz Quadrada(r) -> ").upper())
     
     print("O resultado é ",end="")
     if acao == "A":
@@ -17,18 +19,15 @@ while (rodada > 0):
         print(num1 - num2)
     elif acao == "M":
         print(num1 * num2)
-    else:
+    elif acao == "D":
         print(num1 / num2)
+    else:
+        print(math.sqrt(num1))
 
     print("--------------------------------------------")
 
     deseja_continuar = input("Digite (1) para continuar ou (0) para sair:  ")
     rodada = int(deseja_continuar)
-
-
-
-
-
 
 print("Obrigado por usar meu programa")
 
